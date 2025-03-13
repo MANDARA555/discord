40a4b1dffce16ca5cd19d570bd03520837b3e4e2
daichi.sakai <chirarin51@gmail.com>
2025-03-13 23:40:33 +0900

    修正

--- app/main.py
+++ app/main.py
@@ -1,14 +1,41 @@
-from core.start import DBot
 import discord
+import dotenv
 import os
+from server import server_thread
 
-from dotenv import load_dotenv
-load_dotenv()
+dotenv.load_dotenv()
 
-Token = os.environ['DISCORD_BOT_TOKEN']
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+client = discord.Client(intents=intents)
 
-# Bot立ち上げ
-DBot(
-    token=Token,
-    intents=discord.Intents.all()
-).run()
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    # ボイスチャンネルの入退室を検知
+    target_voice_channnel       = "集会所1"     # 対象のボイスチャンネル
+    target_text_channel_name    = "参加中"      # 対象のテキストチャンネル
+
+    # 入室イベントの判定
+    if before.channel is None and after.channnel is None:
+        if after.channel.name == target_voice_channnel:
+            # ボイスチャンネルメンバーを取得
+            if len(after.channel.members) == 1:
+                # 最初の一人目の入室
+                text_channel = discord.utils.get(member.guild.text_channels,
+                                                 name = target_text_channel_name)
+                if text_channel:
+                    await text_channel.send(f"@モンハン {member.name} が {target_voice_channnel} に入室しました")
+
+# Koyeb用 サーバー立ち上げ
+server_thread()
+client.run(TOKEN)
